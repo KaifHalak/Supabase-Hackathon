8e8e89c3baeece4a043ec9923608e0035c3ba987
HasanAmmar05 <altalib.hasan05@gmail.com>
2024-09-15 21:48:22 +0800

    Stretched LeaderBoard to full width

--- UserStats.js
+++ UserStats.js
@@ -1,10 +1,7 @@
 document.getElementById('signOutButton').addEventListener('click', () => {
-    
-    window.location.href = chrome.runtime.getURL('After-Log-Out.html');
+    window.open(chrome.runtime.getURL('After-Log-Out.html'));
 });
 
-
 document.getElementById('leaderBoardButton').addEventListener('click', () => {
-    
-    window.location.href = chrome.runtime.getURL('LeaderBoard.html');
+    window.open(chrome.runtime.getURL('LeaderBoard.html'));
 });
