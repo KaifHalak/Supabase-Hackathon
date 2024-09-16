2dc5732cb7e0e5aa8c6b21a8dc3f8df6438a3c7f
HasanAmmar05 <altalib.hasan05@gmail.com>
2024-09-16 21:32:37 +0800

    Added the background script (background script and content script as well as a new LeaderBoard design

---
+++ content.js
@@ -0,0 +1,33 @@
+let CurrentVideoId = null;
+let WatchedPercentage = 0;
+let ThresholdReached = false;
+const THRESHOLD = 33;
+
+function updateWatchedPercentage() {
+  const video = document.querySelector("video");
+  if (video && video.duration) {
+    WatchedPercentage = (video.currentTime / video.duration) * 100;
+
+    if (WatchedPercentage >= THRESHOLD && !ThresholdReached) {
+      ThresholdReached = true;
+      chrome.runtime.sendMessage({
+        type: "WATCHED_THRESHOLD",
+        VideoId: CurrentVideoId,
+        percentage: Math.round(WatchedPercentage),
+      });
+    }
+  }
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === "NEW_VIDEO") {
+    CurrentVideoId = request.VideoId;
+    WatchedPercentage = 0;
+    ThresholdReached = false;
+
+    const video = document.querySelector("video");
+    if (video) {
+      video.addEventListener("timeupdate", updateWatchedPercentage);
+    }
+  }
+});
