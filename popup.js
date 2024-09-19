81d0188bccdc7e9f3ba39c178d7b9c229081225a
HasanAmmar05 <altalib.hasan05@gmail.com>
2024-09-19 22:55:21 +0800

    Leaderboard design not finished yet

--- popup.js
+++ popup.js
@@ -1,38 +1,37 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const loginScreen = document.getElementById('loginScreen');
-    const statsScreen = document.getElementById('statsScreen');
-    const loginButton = document.getElementById('loginButton');
-    const logoutButton = document.getElementById('logoutButton');
-    const viewStatsButton = document.getElementById('ViewStatsBtn');
+document.addEventListener("DOMContentLoaded", () => {
+    const loginScreen = document.getElementById("loginScreen");
+    const statsScreen = document.getElementById("statsScreen");
+    const loginButton = document.getElementById("loginButton");
+    const signOutButton = document.getElementById("signOutButton");
+    const leaderBoardButton = document.getElementById("leaderBoardButton");
   
-
     if (loginScreen && statsScreen) {
-        loginScreen.style.display = 'flex';
-        statsScreen.style.display = 'none';
+      loginScreen.style.display = "flex";
+      statsScreen.style.display = "none";
     }
   
-
     if (loginButton) {
-        loginButton.addEventListener('click', () => {
-            if (loginScreen && statsScreen) {
-                loginScreen.style.display = 'none';
-                statsScreen.style.display = 'block';
-            }
-        });
+      loginButton.addEventListener("click", () => {
+        if (loginScreen && statsScreen) {
+          loginScreen.style.display = "none";
+          statsScreen.style.display = "block";
+        }
+      });
     }
   
-
-    if (logoutButton) {
-        logoutButton.addEventListener('click', () => {
-            chrome.tabs.create({ url: 'After-Log-Out.html' });
-        });
+    if (signOutButton) {
+      signOutButton.addEventListener("click", () => {
+        // Handle sign out logic here
+        if (loginScreen && statsScreen) {
+          statsScreen.style.display = "none";
+          loginScreen.style.display = "flex";
+        }
+      });
     }
   
-
-
-    if (viewStatsButton) {
-        viewStatsButton.addEventListener('click', () => {
-            chrome.tabs.create({ url: 'UserStats.html' }); 
-        });
+    if (leaderBoardButton) {
+      leaderBoardButton.addEventListener("click", () => {
+        chrome.tabs.create({ url: "LeaderBoard.html" });
+      });
     }
-});
+  });
