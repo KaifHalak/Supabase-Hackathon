340c415411e6249c27f1c423c004e0120221b52e
usman <usman12k2@gmail.com>
2024-09-22 16:56:04 +0400

    Changed color of leaderboard

--- server/src/controllers/pages.js
+++ server/src/controllers/pages.js
@@ -7,19 +7,19 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const PAGES_PATH = path.join(__dirname, "../../../", "public")
 
 export function leaderboardPage(req, res, next) {
-     res.sendFile(path.join(PAGES_PATH, "leaderboard.html"))
+     res.sendFile(path.join(PAGES_PATH, "src/leaderboard.html"))
 }
 
 export async function leaderboardData(req, res, next) {
      const { data: userArr, error: uError } = await supabase
           .from("Users")
           .select("username, total_points, level")
-          .order("total_points", { ascending: false });
+          .order("total_points", { ascending: false })
 
      return res.json(userArr)
 }
 
 export function mainPage(req, res, next) {
      console.log(req.user)
-     res.sendFile(path.join(PAGES_PATH, "main.html"))
+     res.sendFile(path.join(PAGES_PATH, "src/main.html"))
 }
